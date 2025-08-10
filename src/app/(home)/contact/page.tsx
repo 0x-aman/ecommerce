6e5b98945e47802d7261ba64175860f3c34ff009
WebDevAman <amanpande101@gmail.com>
2025-08-10 13:53:13 +0530

    03: home layout

---
+++ src/app/(home)/contact/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface ContactProps {
+  // Your prop types here
+}
+
+const Contact: FC<ContactProps> = (props) => {
+  return <div></div>;
+};
+
+export default Contact;
